ced164327acb22e35a89f75e379b0753ebc14393
thomas59570 <lahayethomas0@gmail.com>
2024-09-19 09:29:19 +0200

    Update forme.py

--- forme.py
+++ forme.py
@@ -146,6 +146,20 @@ def rectangle(x,y,w,h):
         left(90)
         forward(h)
         left(90)
+
+def trait(x1,y1,x2,y2):
+    print("\nTrait file")
+    '''
+    Paramètres
+        x1, y1 : coordonnées du début du trait
+        x2, y2 : coordonnées de la fin du trait
+    Cette function dessine un trait entre les 2 points transmis en paramètres
+    '''
+    
+    up()
+    goto(x1, y1)
+    down()
+    goto(x2, y2)
     
 # Instructions du programme principal
  
