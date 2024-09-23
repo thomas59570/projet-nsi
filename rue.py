b6183a741f1f8d9c789d8733e5f5c92d0e57bfce
thomas59570 <lahayethomas0@gmail.com>
2024-09-23 15:07:09 +0200

    Update rue.py

--- rue.py
+++ rue.py
@@ -93,5 +93,83 @@ bat.end_fill()
 turtle.done()
 
 
-
+ciel étoilé:
+import turtle
+import random
+  
+  
+# creating turtle object
+t = turtle.Turtle()
+  
+# to activate turtle graphics Screen
+w = turtle.Screen()
+  
+# setting speed of turtle
+t.speed(300)
+  
+# giving the background color of turtle
+# graphics screen
+w.bgcolor("black")
+  
+# giving the color of pen to our turtle
+# for drawing
+t.color("white")
+  
+# giving title to our turtle graphics window
+w.title("Starry Sky")
+  
+# making function to draw the stars
+def stars():
+    for i in range(5):
+        t.fd(10)
+        t.right(144)
+  
+  
+# loop for making number of stars
+for i in range(90):
+    
+    # generating random integer values for x and y
+    x = random.randint(-640, 640)
+    y = random.randint(-330, 330)
+      
+    # calling the function stars to draw the 
+    # stars at random x,y value
+    stars()
+      
+    # took up the turtle's pen
+    t.up()
+      
+    # go at the x,y coordinate generated above
+    t.goto(x, y)
+      
+    # took down the pen to draw
+    t.down()
+  
+# for making our moon tooking up the pen
+t.up()
+  
+# going at the specific coordinated
+t.goto(0, 170)
+  
+# took down the pen to start drawing
+t.down()
+  
+# giving color to turtle's pen
+t.color("white")
+  
+# start filling the color
+t.begin_fill()
+  
+# making our moon
+t.circle(40)
+  
+# stop filling the color
+t.end_fill()
+  
+# after drawing hidding the turtle from
+# the window
+t.hideturtle()
+  
+# terminated the window after clicking
+w.exitonclick()
 
