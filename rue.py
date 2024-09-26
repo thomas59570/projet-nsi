e0ee6833c032470927e4c9dd60da4ed07e840638
NATHANNEVETB <nathanneve16@gmail.com>
2024-09-26 08:24:14 +0200

    Update rue.py

--- rue.py
+++ rue.py
@@ -201,3 +201,33 @@ def immeuble(x, y_sol):
 
     # Dessin du toit
     toit(x, y_sol, nb_etage+1)
+
+
+
+
+
+import turtle
+from random import randint, shuffle
+from sol import sol
+from immeuble import immeuble
+
+def main():
+    print("Main file\n")
+
+    turtle.clear()
+    turtle.setup(800, 600)
+    turtle.speed(0)
+
+    # On définit la hauteur du sol
+    y_sol = -200
+
+    # Dessin du sol de la rue
+    sol(y_sol)
+
+    # Dessin des 4 immeubles
+    for i in range(4):
+      immeuble(-300+(i*300), y_sol)
+
+    # On ferme la fenêtre s'il y a un clique gauche
+    turtle.exitonclick()
+
